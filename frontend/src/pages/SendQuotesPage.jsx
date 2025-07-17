d33c37080069654ad8005b4b90443fc4c004e019
Shreya Garg <shreyag.garg72@gmail.com>
2025-07-17 18:35:04 +0530

    order generated once, toggle between send quote and update quote status,added accept decline button

--- frontend/src/pages/SendQuotesPage.jsx
+++ frontend/src/pages/SendQuotesPage.jsx
@@ -327,7 +327,7 @@ const SendQuotesPage = () => {
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
               >
-                Completed Inquiries ({completedInquiries.length})
+                Send Quote to Comapany ({completedInquiries.length})
               </button>
               <button
                 onClick={() => setActiveTab("fulfilled")}
@@ -337,7 +337,7 @@ const SendQuotesPage = () => {
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
                 }`}
               >
-                Fulfilled Inquiries ({fulfilledInquiries.length})
+                Pending Updates ({fulfilledInquiries.length})
               </button>
             </nav>
           </div>
