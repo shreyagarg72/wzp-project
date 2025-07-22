ccde332e72fbeeedad7a0c3c21cbcb7bc0ef60bd
Shreya Garg <shreyag.garg72@gmail.com>
2025-07-22 19:12:03 +0530

    admin dashboard fix

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,19 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const quoteItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  brand: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  category: { type: String },
-  description: { type: String },
-  specifications: { type: String },
-  uom: { type: String },
-  basePrice: { type: Number, required: true },
-  margin: { type: Number },
- discount: { type: Number },
-  gstRate: { type: Number},
-  finalPrice: { type: Number, required: true }
-}, { _id: false });
+const quoteItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: String },
+    description: { type: String },
+    specifications: { type: String },
+    uom: { type: String },
+    basePrice: { type: Number, required: true },
+    margin: { type: Number },
+    discount: { type: Number },
+    gstRate: { type: Number },
+    finalPrice: { type: Number, required: true },
+  },
+  { _id: false }
+);
 
 const orderSchema = new mongoose.Schema({
   orderId: { type: String, unique: true },
@@ -21,9 +24,12 @@ const orderSchema = new mongoose.Schema({
   items: [quoteItemSchema],
   deliveryCharges: { type: Number, default: 0 },
   totalPrice: { type: Number, required: true },
-  status: { type: String, enum: ['Open','Sent','Accept', 'Negotiation', 'Decline'] },  
-  createdAt: { type: Date, default: Date.now }
+  status: {
+    type: String,
+    enum: ["Open", "Sent", "Accept", "Negotiation", "Decline"],
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 export default Order;
